1f9080c539bf466ec347ad234640c0b23a7cabc2
petr <petyalom@yandex.ru>
2023-10-24 00:04:08 +0300

    first commit

---
+++ src/main/groovy/ru/ravel/pricechecker/PriceCheckerApplication.groovy
@@ -0,0 +1,13 @@
+package ru.ravel.pricechecker
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PriceCheckerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PriceCheckerApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ru/ravel/pricechecker/PriceCheckerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ru.ravel.pricechecker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PriceCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
